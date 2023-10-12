501c57e468ca0a46476eb93c8e1d056c3d7a9be5
Aboba1001Alt <146593705+Aboba1001Alt@users.noreply.github.com>
2023-10-12 23:44:15 +0400

    Update VisualsUISubState.hx

--- source/options/VisualsUISubState.hx
+++ source/options/VisualsUISubState.hx
@@ -47,6 +47,13 @@ class VisualsUISubState extends BaseOptionsMenu
 			'bool',
 			false);
 		addOption(option);
+
+		var option:Option = new Option('Show Timing Counter',
+			'If checked, a offset (in ms) will be displayed.',
+			'showMsText',
+			'bool',
+			true);
+		addOption(option);
 		
 		var option:Option = new Option('Time Bar:',
 			"What should the Time Bar display?",
